ec6332ba2a0d8c16049b2de479b44a19704e5fba
muntazar77 <muntadher.attar@gmail.com>
2026-03-31 00:03:45 +0200

    refactor(account, trash): simplify AccountViewPage and TrashViewPage components by removing DefaultTemplate

--- src/components/admin/account/AccountViewPage.tsx
+++ src/components/admin/account/AccountViewPage.tsx
@@ -1,20 +1,5 @@
-import { DefaultTemplate } from '@payloadcms/next/templates'
-import type { AdminViewServerProps } from 'payload'
 import AccountView from './AccountView'
 
-export default function AccountViewPage(props: AdminViewServerProps) {
-  return (
-    <DefaultTemplate
-      i18n={props.i18n}
-      locale={props.locale}
-      params={props.params}
-      payload={props.payload}
-      permissions={props.permissions}
-      searchParams={props.searchParams}
-      user={props.user}
-      visibleEntities={props.initPageResult.visibleEntities}
-    >
-      <AccountView />
-    </DefaultTemplate>
-  )
+export default function AccountViewPage() {
+  return <AccountView />
 }
--- src/components/admin/trash/TrashViewPage.tsx
+++ src/components/admin/trash/TrashViewPage.tsx
@@ -1,21 +1,5 @@
-import React from 'react'
-import { DefaultTemplate } from '@payloadcms/next/templates'
-import type { AdminViewServerProps } from 'payload'
 import TrashView from './TrashView'
 
-export default function TrashViewPage(props: AdminViewServerProps) {
-  return (
-    <DefaultTemplate
-      i18n={props.i18n}
-      locale={props.locale}
-      params={props.params}
-      payload={props.payload}
-      permissions={props.permissions}
-      searchParams={props.searchParams}
-      user={props.user}
-      visibleEntities={props.initPageResult.visibleEntities}
-    >
-      <TrashView />
-    </DefaultTemplate>
-  )
+export default function TrashViewPage() {
+  return <TrashView />
 }
